ccb11e28134d65bc07da88240367178a70df91b6
Ganesh Tiwari <ganesht049@gmail.com>
2023-09-26 23:58:29 +0530

    feat: added contact me form

--- src/routes/send-ping/+page.svelte
+++ src/routes/send-ping/+page.svelte
@@ -1,10 +1,30 @@
 <head>
-  <title>
-    Contact Ganesh Somehow
-  </title>
+	<title> Contact Ganesh Somehow </title>
 </head>
 <div class="my-8">
-  <h2>
-    I will add something here so that you can send some message to me directly
-  </h2>
+	<h2>I will add something here so that you can send some message to me directly</h2>
+
+	<form action="" class="flex flex-col gap-4 my-8">
+		<label for="name"> Name </label>
+		<input type="text" name="name" id="name" placeholder="Your Name" 
+      class="border p-2 dark:text-black" />
+
+		<label for="email"> Email </label>
+		<input type="email" name="email" id="email" placeholder="Your Email" 
+      class="border p-2 dark:text-black" />
+
+		<label for="message"> Message </label>
+		<textarea
+			name="message"
+			id="message"
+			cols="30"
+			rows="10"
+			placeholder="Your Message"
+			class="border p-2 dark:text-black"
+		/>
+
+    <div class="cf-turnstile" data-sitekey="0x4AAAAAAAKu0NExklA4FHOm"></div>
+    
+		<button type="submit" class="border py-4"> Send Message </button>
+	</form>
 </div>
